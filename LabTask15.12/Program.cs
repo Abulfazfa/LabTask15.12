7f96bbeb87003e7de25ea2574156377d1535f676
unknown <abulfazfa@code.edu.az>
2022-12-17 10:55:25 +0400

    FirstAttempt

---
+++ LabTask15.12/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+
+namespace LabTask15._12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please sellect a digit which it contain between 0 and 6;");
+            Menu(Convert.ToInt32(Console.ReadLine()));
+
+        }
+
+        public static void Menu(int num)
+        {
+            Student student = new Student();
+            student.studentList.Add(student);
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Please enter student's fullname");
+                    student.FullName = Console.ReadLine();       
+                    break;
+                case 2:
+                    Console.WriteLine("Please enter student's number, fullname and point");
+                    int no = Convert.ToInt32(Console.ReadLine());
+                    string name = Console.ReadLine();
+                    int point = Convert.ToInt32(Console.ReadLine());
+                    student.AddExam(no, name, point);
+                    break ;
+                case 3:
+                    Console.WriteLine("Please enter student's number and fullname");
+                    student.GetExamResult(student.No, student.FullName);
+                    break ;
+                case 4:
+                    Console.WriteLine("Please enter student's number");
+                    student.GetExamsResults(student.No);
+                    break;
+                case 5:
+                    Console.WriteLine("Please enter student's number");
+                    Console.WriteLine(student.GetExamAvg(student.No));
+                    break;
+                case 6:
+                    Console.WriteLine("Please enter student's number and fullname");
+                    Console.WriteLine(student.RemoveExam(student.No, student.FullName)); 
+                    break;
+                case 0:
+                    
+                    break;             
+                default:          
+                    break;
+            }
+        }
+    }
+}
---
+++ LabTask15.12/Student.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LabTask15._12
+{
+    internal class Student 
+    {
+        public  int No { get; set; }
+        public string FullName { get; set; }
+        public Dictionary<string, int> Exams { get; set; }
+
+        public List<Student> studentList = new List<Student>();
+        
+        public Student()
+        {
+            Exams = new Dictionary<string, int>();
+        }
+        
+
+        public void AddExam(int no, string name, int point)
+        {
+            Exams.Add(name, point);
+        }
+
+        public void GetExamResult(int no, string name)
+        {
+            foreach (var student in studentList)
+            {
+                if (no == student.No)
+                {
+                    foreach (var exam in Exams)
+                    {
+                        if (name == exam.Key)
+                        {
+                            Console.WriteLine(exam.Value);
+                            break;
+                        }
+                    }
+                }
+            }
+            
+        }
+ 
+        public double GetExamAvg(int no)
+        {
+            foreach (var item in studentList)
+            {
+                if (no == item.No)
+                {
+                  return Exams.Average(a => a.Value);
+                }
+            }
+            return -1;
+           
+        }
+
+        public void GetExamsResults(int no)
+        {
+            foreach (var student in studentList)
+            {
+                if (no == student.No)
+                {
+                    foreach (var exam in Exams)
+                    {
+                        Console.WriteLine(exam);
+                        break;
+                    }
+                }
+            }
+        }
+
+        public Dictionary<string, int> RemoveExam(int no, string name) 
+        {
+            Dictionary<string, int> result = new Dictionary<string, int>();
+            foreach (var student in studentList)
+            {
+                if (no == student.No)
+                {
+                    foreach (var exam in Exams)
+                    {
+                        if (name != exam.Key)
+                        {
+                            result.Add(exam.Key, exam.Value);
+                        }
+                    }
+                }
+            }
+            return null;
+
+
+        }
+
+    
+    }
+}
